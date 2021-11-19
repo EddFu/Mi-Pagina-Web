bca70b086942bf467d1b230e762e2270ab2821cb
Eduardo Fuentes <esteben305@gmail.com>
2021-11-19 14:56:33 -0500

    agregue cambios al menu de navegacion con js para que tenga funcionalidad

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('.menu');
+
+const menuNavegacion = document.querySelector('.menu-navegacion');
+
+console.log(menu);
+console.log(menuNavegacion);
+
+menu.addEventListener('click', ()=> {
+    menuNavegacion.classList.toggle("spread")
+});
+
+window.addEventListener('click', e=>{
+    // console.log(e.target)
+    if(menuNavegacion.classList.contains('spread') && e.target != menuNavegacion && e.target !=menu){
+            
+            menuNavegacion.classList.toggle("spread")
+    }
+})
